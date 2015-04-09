0bee0ea65f000bdf7e87674da39b7119a192b6a9
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-08 21:39:17 -0400

    The routes work, no errors

---
+++ contact.rb
@@ -0,0 +1,14 @@
+class Contact
+    attr_accessor :first_name, :last_name, :email, :note, :id
+
+    def initialize(first_name, last_name, email, note)
+        @first_name = first_name
+        @last_name = last_name
+        @email = email
+        @note = note
+    end
+
+    def to_s
+        print "First name: #{@first_name}, Last_name: #{@last_name}, Email: #{@email}, Notes: #{@note}"
+    end
+end
--- crm.rb
+++ crm.rb
@@ -1,3 +1,4 @@
+require_relative 'contact'
 require 'sinatra'
 
 get '/' do
@@ -6,6 +7,10 @@ get '/' do
 end
 
 get '/contacts' do
+    # @contacts = []
+    # @contacts << Contact.new("name1", "lastname1", "email1", "note1")
+    # @contacts << Contact.new("name2", "lastname2", "email2", "note2")
+    # @contacts << Contact.new("name3", "lastname3", "email3", "note3")
     erb :contacts
 end
 
